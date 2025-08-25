606c990082531934db28f3131307af6490ab4afc
sangbate258 <sangbate258@gmail.com>
2025-08-25 19:23:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,65 +1,71 @@
-const courses = [
-    {
-    id: '1',
-    name: '1',
-    coin: 1
-    },
-    {
-    id: '2',
-    name: '2',
-    coin: 2
-    },
-    {
-    id: '3',
-    name: '3',
-    coin: 3
-    },
-    {
-    id: '4',
-    name: '4',
-    coin: 4
-    },
-    {
-    id: '4',
-    name: '4',
-    coin: 4
-    },
-    {
-    id: '5',
-    name: '5',
-    coin: 5
-    },
-    {
-    id: '6',
-    name: '6',
-    coin: 6
-    },
-    {
-    id: '7',
-    name: '7',
-    coin: 7
-    },
-]
+// const courses = [
+//     {
+//     id: '1',
+//     name: '1',
+//     coin: 1
+//     },
+//     {
+//     id: '2',
+//     name: '2',
+//     coin: 2
+//     },
+//     {
+//     id: '3',
+//     name: '3',
+//     coin: 3
+//     },
+//     {
+//     id: '4',
+//     name: '4',
+//     coin: 4
+//     },
+//     {
+//     id: '4',
+//     name: '4',
+//     coin: 4
+//     },
+//     {
+//     id: '5',
+//     name: '5',
+//     coin: 5
+//     },
+//     {
+//     id: '6',
+//     name: '6',
+//     coin: 6
+//     },
+//     {
+//     id: '7',
+//     name: '7',
+//     coin: 7
+//     },
+// ]
 
-function changeArray(courses)
-{
-    return {
-        id: courses.id,
-        name: `Khoa hoc ${courses.name}`,
-        coin: courses.coin,
-        cointText: `Gia: ${courses.coin}`
-    }
-}
-const newCourses = courses.map(changeArray);
+// function changeArray(courses)
+// {
+//     return {
+//         id: courses.id,
+//         name: `Khoa hoc ${courses.name}`,
+//         coin: courses.coin,
+//         cointText: `Gia: ${courses.coin}`
+//     }
+// }
+// const newCourses = courses.map(changeArray);
 // console.log(newCourses);
 
 //chi lay name trong mang cu sang mang moi
 
-function giCungDuoc(course)
-{
-    return {
-        name: course.name
-    }
-}
-const newCoursesName = courses.map(giCungDuoc)
-console.log(newCoursesName)
+// function giCungDuoc(course)
+// {
+//     return {
+//         name: course.name
+//     }
+// }
+// const newCoursesName = courses.map(giCungDuoc)
+// console.log(newCoursesName)
+
+//double số trong mảng
+// const arrayNumbers = [1,2,3,4,5];
+
+// const newArray = arrayNumbers.map(element => element*2);
+// console.log(newArray);
